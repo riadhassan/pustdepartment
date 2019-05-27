5c7da2770ed0f04bb528ae3992e617b8d0072f25
riadhassan <riad_hassan@outlook.com>
2019-05-27 19:27:51 +0600

    fix 404

--- 404.php
+++ 404.php
@@ -1,29 +1,26 @@
+
+
 <?php
 get_header();
-?>
-<div style="height: 200px">
+?><link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-</div>
-<div class="container">
-    <div class="row" style="padding-left: 100px">
-        <div class="col-md-12">
-            <div class="error-template">
-                <h1>
-                    Oops!</h1>
-                <h2>
-                    Not Found</h2>
-                <div class="error-details">
-                    Sorry, This page under construction!
-                </div>
-                <div class="error-actions">
-                    <a href="<?php echo get_bloginfo('url') ?>" class="btn btn-primary btn-lg">Take Me Home</a>
-                </div>
+<div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <div style="margin-top: 200px; margin-bottom: 100px">
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 lead">The page you are looking for was not found.</div>
+                <a href="<?php echo get_bloginfo('url') ?>" class="btn btn-outline-success">Back to Home</a>
+            </div>
             </div>
         </div>
     </div>
 </div>
-    <div style="height: 100px">
 
-    </div>
 <?php
 get_footer();
+?>
